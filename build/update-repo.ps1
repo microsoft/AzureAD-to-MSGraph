2e6a241735e88fb3daf8045fa1720cf77431fbcd
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2023-02-09 00:32:03 +0100

    CI_UPDATE

---
+++ build/update-repo.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding()]
+param (
+	[Parameter(Mandatory = $true)]
+	[string]
+	$GitToken,
+
+	[Parameter(Mandatory = $true)]
+	[string]
+	$AccountName,
+
+	[Parameter(Mandatory = $true)]
+	[string]
+	$RepositoryName,
+
+	[string]
+	$Message = 'CI_UPDATE'
+)
+
+$repositoryRoot = Split-Path $PSScriptRoot
+Push-Location -Path $repositoryRoot
+git add .
+git commit -a -m $Message
+git push "https://$GitToken@github.com/$AccountName/$RepositoryName.git"
+Pop-Location
